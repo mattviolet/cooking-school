6827349687bc8ccc8c5b49e414678cece1508349
Matthew Violet <mattviolet123@gmail.com>
2020-11-06 16:10:57 -0500

    removed cookie parser module to see if it caused error

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var port = process.env.PORT || 5000;
-var cookieParser = require("cookie-parser");
+//var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var cors = require("cors");
@@ -36,7 +36,7 @@ app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(cookieParser());
 app.use(express.static(publicPath));
 
 app.listen(port, () => {
