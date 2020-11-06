c8039b81a0b51a40e4aa635cf1c6ce5a8412f924
Matthew Violet <mattviolet123@gmail.com>
2020-11-06 16:36:04 -0500

    EJECT

---
+++ src/build/precache-manifest.e5061c563e2e4405f1dfe5c1da5313ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e82ede8f397875b98aaad8cf7be8cc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9dd597b568e43542a34",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "66de3ede953e8f49fb66",
+    "url": "/static/css/main.cdbd58e2.chunk.css"
+  },
+  {
+    "revision": "d9dd597b568e43542a34",
+    "url": "/static/js/2.8b749e19.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8b749e19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66de3ede953e8f49fb66",
+    "url": "/static/js/main.8380c875.chunk.js"
+  },
+  {
+    "revision": "7f6ebf68b5223ceeee53",
+    "url": "/static/js/runtime-main.ad2ba3a6.js"
+  },
+  {
+    "revision": "6198c1a1aebc7f5dfdfc0ac79e9469c8",
+    "url": "/static/media/school_cover_picture_1.6198c1a1.jpg"
+  }
+]);
